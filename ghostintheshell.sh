17d334e3adb9949f6cd8c83360744a5fe6a4403d
RTFM <118631549+RTFM-blackrobe@users.noreply.github.com>
2024-06-14 11:28:52 -0500

    Update ghostintheshell.sh

--- ghostintheshell.sh
+++ ghostintheshell.sh
@@ -4,7 +4,7 @@
 sudo systemsetup -setremotelogin off
 
 # Disable screen sharing
-sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool 
+sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool
 true
 
 # Disable file sharing
@@ -17,5 +17,40 @@ sudo defaults write /Library/Preferences/com.apple.RemoteManagement.plist ARD_Al
 sudo defaults write /Library/Preferences/com.apple.RemoteManagement.plist DisabledRemoteDesktop -bool true
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
 sudo launchctl unload -w /System/Library/LaunchAgents/com.apple.RemoteUI.plist
+# Block SSH
+sudo launchctl unload -w /System/Library/LaunchDaemons/ssh.plist
 
+# Block FTP
+sudo launchctl unload -w /System/Library/LaunchDaemons/ftp.plist
+
+# Block Telnet
+sudo launchctl unload -w /System/Library/LaunchDaemons/telnet.plist
+
+# Block TFTP
+sudo launchctl unload -w /System/Library/LaunchDaemons/tftp.plist
+
+# Block NetBIOS
+sudo launchctl unload -w /System/Library/LaunchDaemons/netbios.plist
+
+# Block NPC (NetBIOS Name Service)
+sudo launchctl unload -w /System/Library/LaunchDaemons/nmbd.plist
+
+sudo launchctl unload -w /System/Library/LaunchDaemons/org.cups.cupsd.plist
+
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.blued.plist
+
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.remotepairtool.plist
+
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.ppd.plist
+
+defaults write com.apple.NetworkBrowser DisableAirDrop -bool YES
+
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+
+
+#block all incoming traffic aplication layer firewall
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 2
+
+#port 22 block
+sudo pfctl -t blocklist -T add 0.0.0.0/0 port 22
 
